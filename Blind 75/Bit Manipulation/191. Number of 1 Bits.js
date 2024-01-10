6c20f6a3a546c00164b833e012e9f0aed1f0039a
ReshmaGhanta <reshma.ghanta@gmail.com>
2024-01-09 20:07:29 -0500

    Bit manipulation

--- Blind 75/Bit Manipulation/191. Number of 1 Bits.js
+++ Blind 75/Bit Manipulation/191. Number of 1 Bits.js
@@ -2,11 +2,14 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-var hammingWeight = function(n) {
-    let count=0;
-    for(let i=0;i<32;i++){
-        count +=n&1;
-        n=n>>1;
+var hammingWeight = function (n) {
+    let count = 0;
+    for (let i = 0; i < 32; i++) {
+      let val= n & 1;
+       if(val==1){
+           count++;
+       }
+      n = n >> 1;
     }
- return count;
-};
+    return count;
+  };
