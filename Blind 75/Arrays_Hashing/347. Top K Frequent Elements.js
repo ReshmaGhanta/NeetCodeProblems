ce549c43a6e8b9489e5a4d1d6bed7d953204509e
Reshma Ghanta <45803247+ReshmaGhanta@users.noreply.github.com>
2024-03-19 13:57:43 -0400

    Update 347. Top K Frequent Elements.js

--- Blind 75/Arrays_Hashing/347. Top K Frequent Elements.js
+++ Blind 75/Arrays_Hashing/347. Top K Frequent Elements.js
@@ -1,1 +1,36 @@
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let numsCount={};
+    let freqElements=[];
+    for(let i=0;i<nums.length;i++){
+        let count=nums[i];
+         if(numsCount[count]){
+            numsCount[count]++
+         }else{
+            numsCount[count]=1
+         }
+      }
+
+let bucket=[];
+
+for(let i=0;i<=nums.length;i++){
+      bucket.push([]);
+}
+for (const key in numsCount) {
+   bucket[numsCount[key]].push(parseInt(key));
+}
+   for(let i=bucket.length-1;i>=0;i--){
+       if(bucket[i].length>0){
+        freqElements=[...freqElements,...bucket[i]];
+        if(freqElements.length===k){
+            return freqElements;
+        }
+       }
+   }
+
+};
